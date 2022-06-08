35589079698828e659278f6e9a2944be523aa855
stan1i <alexandersamedov@gmail.com>
2022-06-08 12:29:03 +0300

    refactor gsm

--- services/questionService.js
+++ services/questionService.js
@@ -5,11 +5,11 @@ class QuestionService{
 
     checkRoundCoordinate(valueAnswer, valueOrigin){
         const valueAnswerRound = Math.round(valueAnswer*1000)
-        const valueOriginMin = Math.round(valueOrigin*1000 - 1)
-        const valueOriginMax = Math.round(valueOrigin*1000 + 1)
+        const valueOriginMin = Math.round(valueOrigin*1000 - 2)
+        const valueOriginMax = Math.round(valueOrigin*1000 + 2)
         console.log(valueOriginMin, valueOriginMax)
         console.log(valueAnswerRound)
-        return valueOriginMin < valueAnswerRound && valueAnswerRound < valueOriginMax
+        return valueOriginMin <= valueAnswerRound && valueAnswerRound <= valueOriginMax
     }
 
     async checkAnswerGeo(userId, quizId, answer){
