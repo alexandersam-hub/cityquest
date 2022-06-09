94cc63ab9606415424c158a09122b024c13205f9
stan1i <alexandersamedov@gmail.com>
2022-06-09 19:39:41 +0300

    add puth round 2

--- services/questionService.js
+++ services/questionService.js
@@ -4,9 +4,9 @@ const quizService = require('./quizService')
 class QuestionService{
 
     checkRoundCoordinate(valueAnswer, valueOrigin){
-        const valueAnswerRound = Math.round(valueAnswer*10000)
-        const valueOriginMin = Math.round(valueOrigin*10000) - 2
-        const valueOriginMax = Math.round(valueOrigin*10000 )+ 2
+        const valueAnswerRound = Math.round(valueAnswer*100000)
+        const valueOriginMin = Math.round(valueOrigin*100000) - 40
+        const valueOriginMax = Math.round(valueOrigin*100000 )+ 40
         // console.log(valueOriginMin, valueOriginMax)
         // console.log(valueAnswerRound)
         return valueOriginMin <= valueAnswerRound && valueAnswerRound <= valueOriginMax
