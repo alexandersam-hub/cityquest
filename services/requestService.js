f00281001a7527d8dc4ba33f14cb767b91e26dfd
stan1i <alexandersamedov@gmail.com>
2022-06-14 14:48:47 +0300

    add request promocode v5

--- services/requestService.js
+++ services/requestService.js
@@ -1,20 +1,19 @@
-const request= require('request')
+const messenger= require('messenger')
+const client = messenger.createSpeaker(8100);
 
 class RequestService{
 
     async pullPromoToQuizServer(code, userData){
         try{
-            request({
-                url:process.env.URL_QUIZ_SERVER+'/api/promo/pull',
-                method:'POST',
-                json: true,
-                body:{
-                    code,
-                    user_data:JSON.stringify(userData)
-                },
-                headers:{ 'Content-Type': 'application/json'}
-            }).then(res => res.json()).then(json=>{
-                return !json.warning;
+            client.request('give promo', {code, userData}, data=>{
+                if(!data.warning)
+                    return true
+                else{
+                    setTimeout(
+                        ()=>this.pullPromoToQuizServer(), 1000
+                    )
+
+                }
             })
         }catch (e) {
             console.log(e)
