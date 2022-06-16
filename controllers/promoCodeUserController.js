27852589715376d6f6a4311d3d242b365c07d26b
stan1i <alexandersamedov@gmail.com>
2022-06-16 10:24:26 +0300

    add promo v7

--- controllers/promoCodeUserController.js
+++ controllers/promoCodeUserController.js
@@ -5,8 +5,10 @@ class PromoCodeUserController {
     async getPromo(req,res){
         try{
             const {token, quiz} = req.body
+
             const user = tokenService.validationToken(token)
-            const result = userPromoCodeService.getCodeByUserId(quiz, user)
+            console.log(token, quiz,user)
+            const result = userPromoCodeService.getCodeByUserId(quiz, user.id)
             return res.json({warning:false, code:result})
         }catch (e) {
             return res.json({warning:true, message:'Ошибка сервиса'})
